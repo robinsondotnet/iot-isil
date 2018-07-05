2957bc1d48bc66da1d15c3597e0e9a68bd3026ce
Robinson V <robinsondotnet@hotmail.com>
2018-07-05 02:06:02 -0500

    cleanup and call backend endpoint

--- pi-client/Program.cs
+++ pi-client/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO.Ports;
+using System.Net.Http;
+using Newtonsoft.Json;
 using RJCP.IO.Ports;
 
 namespace pi_client
@@ -8,56 +10,65 @@ namespace pi_client
     class Program
     {
         static void Main(string[] args)
-    {
-        SerialPortStream myPort = null;
-        Console.WriteLine("Hello Serial!");
-        Console.WriteLine(Environment.Version.ToString());
-        string[] ports = GetPortNames();
-        myPort = new SerialPortStream("/dev/ttyACM0", 9600, 8, Parity.None, StopBits.One);
-        myPort.Open();
-                if (!myPort.IsOpen)
-                {
-                    Console.WriteLine("Error opening serial port");
-                    return;
-                }
-                Console.WriteLine("Port open");
-        myPort.Handshake = Handshake.None;
-        myPort.ReadTimeout = 10000;
-        myPort.NewLine = "\r\n";
-
-        while (!Console.KeyAvailable)
         {
-            try
+            SerialPortStream arduinoPort = null;
+            Console.WriteLine("Hola Proyecto Final IOT");
+            Console.WriteLine(Environment.Version.ToString());
+            //string[] ports = GetPortNames();
+            // We know that ttyACM0 is the port to read from and copied the same read config from node-red arduino config :)
+            arduinoPort = new SerialPortStream("/dev/ttyACM0", 9600, 8, Parity.None, StopBits.One);
+            arduinoPort.Open();
+            if (!arduinoPort.IsOpen)
             {
-                string readed = myPort.ReadLine();
-                Console.WriteLine(readed);
+                Console.WriteLine("Error opening serial port");
+                return;
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error: {ex.Message}");
-            }
+            Console.WriteLine("Port open");
+            arduinoPort.Handshake = Handshake.None;
+            arduinoPort.ReadTimeout = 10000;
+            arduinoPort.NewLine = "\r\n";
 
-        }
-    }
-    public static string[] GetPortNames()
-    {
-        int p = (int)Environment.OSVersion.Platform;
-        List<string> serial_ports = new List<string>();
-
-        // Are we on Unix?
-        if (p == 4 || p == 128 || p == 6)
-        {
-            string[] ttys = System.IO.Directory.GetFiles("/dev/", "tty\\*");
-            foreach (string dev in ttys)
+            while (!Console.KeyAvailable)
             {
-                if (dev.StartsWith("/dev/ttyS") || dev.StartsWith("/dev/ttyUSB") || dev.StartsWith("/dev/ttyACM") || dev.StartsWith("/dev/ttyAMA"))
+                try
                 {
-                    serial_ports.Add(dev);
-                    Console.WriteLine("Serial list: {0}", dev);
+                    string retrievedLine = arduinoPort.ReadLine();
+                    
+                    var lineParts = retrievedLine.Split(',');
+
+                    var parkingId = lineParts[0];
+                    var isUsed = (YesNo) Enum.Parse(typeof(YesNo) , lineParts[1]);
+
+                    var httpClient = new HttpClient();
+
+                    var response = httpClient.GetAsync($"https://us-central1-iot-isil.cloudfunctions.net/startService?parkingId={parkingId}&isUsed={isUsed}")
+                        .Result;
+
+                    var result = JsonConvert.DeserializeObject<ServiceResponse>(response.Content.ReadAsStringAsync().Result);
+
+                    Console.WriteLine(retrievedLine);
                 }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($" :( Error: {ex.Message}");
+                }
+
             }
         }
-        return serial_ports.ToArray();
+
+        #region Plan Objects
+
+        class ServiceResponse 
+        {
+            public bool Success { get; set; }
+            public string Message  { get; set; }
+        }
+
+        enum YesNo {
+            Yes = 1,
+            No = 0
+        }
+
+        #endregion
     }
-}
 }
